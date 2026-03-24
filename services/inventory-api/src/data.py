9f89558c526be193db9e5840b717f2be805913ce
sedna08 <ochiamitcham@gmail.com>
2026-03-24 12:15:13 +0800

    Updated the data and stock api endpoint to return an array of data

--- services/inventory-api/src/data.py
+++ services/inventory-api/src/data.py
@@ -1,5 +1,24 @@
 # Static in-memory dictionary acting as our data store
-_INVENTORY_DATA = {"item": "shoes", "qty": 42}
+_INVENTORY_DATA = [
+    {
+        "id": "1",
+        "name": "Mechanical Keyboard",
+        "stock": 42,
+        "price": 150.00
+    },
+    {
+        "id": "2",
+        "name": "Wireless Mouse",
+        "stock": 8,
+        "price": 45.50
+    },
+    {
+        "id": "3",
+        "name": "USB-C Hub",
+        "stock": 115,
+        "price": 29.99
+    }
+]
 
 
 def get_inventory() -> dict:
